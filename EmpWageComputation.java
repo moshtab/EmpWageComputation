d8a3a268a828b2777d9ae2749a7903f3cbadcc17
moshtab <mdmohsin41439@gmail.com>
2021-07-27 13:20:54 +0530

    adding parttime

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -5,11 +5,14 @@ public class EmpWageComputation {
 	public static void main(String[] args) {
 	
 		int isFullTime = 0;
+		int isparttime = 1;
 		int workingHr=0;
 		int wage_per_hr=20;
-    	int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+    	int empCheck = (int) Math.floor(Math.random() * 10) % 3;
     	if(empCheck == isFullTime)
            workingHr=8;
+    	else if(empCheck == isparttime )
+    		workingHr=4;
     	else
     	   workingHr=0;
     	int Emp_wage = wage_per_hr*workingHr;
