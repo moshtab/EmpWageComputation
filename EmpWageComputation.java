219583dbc908ef7593e0704b91fe2c191d096794
moshtab <mdmohsin41439@gmail.com>
2021-07-27 12:58:54 +0530

    Checking Emp attendance

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,7 +4,12 @@ public class EmpWageComputation {
 
 	public static void main(String[] args) {
 	
-		System.out.println("Welcome to Employee Wage Computation Program");
+		int isFullTime = 0;
+    	int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+    	if(empCheck == isFullTime)
+            System.out.println( "Emp is present");
+    	else
+    		System.out.println("Emp is absent");
 	}
 
 }
